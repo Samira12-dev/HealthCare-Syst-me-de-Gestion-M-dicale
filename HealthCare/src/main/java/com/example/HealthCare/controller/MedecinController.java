abca5f2c309fafbd51133ff05b643ef96cbf3bdf
Samira12-dev <samiraelboussidi663@gmail.com>
2026-04-27 14:51:16 +0100

    termine class controller de medecin

---
+++ HealthCare/src/main/java/com/example/HealthCare/controller/MedecinController.java
@@ -0,0 +1,41 @@
+package com.example.HealthCare.controller;
+
+import com.example.HealthCare.dto.MedecinRequestDTO;
+import com.example.HealthCare.dto.MedecinResponseDTO;
+import com.example.HealthCare.service.MedecinService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/medecins")
+@RestControllerAdvice
+public class MedecinController {
+    private  final MedecinService medecinService;
+
+    public MedecinController(MedecinService medecinService) {
+        this.medecinService = medecinService;
+    }
+
+    @PostMapping
+    public MedecinResponseDTO addMedecin(@Valid  @RequestBody MedecinRequestDTO medecinrequestdto){
+        return medecinService.addMedecin(medecinrequestdto);
+    }
+    @PutMapping("id")
+    public MedecinResponseDTO updateMedecin(@PathVariable Long id, @Valid @RequestBody MedecinRequestDTO medecinRequestDTO){
+        return medecinService.updaMedecin(id,medecinRequestDTO);
+    }
+
+    @DeleteMapping("id")
+    public void  deleteMedecin(@PathVariable Long id){
+        medecinService.deleteMedecin(id);
+    }
+
+    @GetMapping
+    public List<MedecinResponseDTO>getAllMedecin(){
+        return medecinService.getAllMedecin();
+    }
+
+}
