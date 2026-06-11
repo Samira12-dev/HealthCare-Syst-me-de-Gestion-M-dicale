1bd5fa5b22a44c301762ba3a126ea5202337693e
Samira12-dev <samiraelboussidi663@gmail.com>
2026-06-12 00:55:49 +0100

    add EntityGraph to load patient with appointments

--- HealthCare/src/main/java/com/example/HealthCare/repository/DossierMedicalRepo.java
+++ HealthCare/src/main/java/com/example/HealthCare/repository/DossierMedicalRepo.java
@@ -1,9 +1,13 @@
 package com.example.HealthCare.repository;
 
 import com.example.HealthCare.entity.DossierMedical;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DossierMedicalRepo extends JpaRepository<DossierMedical, Long> {
+import java.util.Optional;
 
+public interface DossierMedicalRepo extends JpaRepository<DossierMedical, Long> {
+    @EntityGraph(attributePaths = "patient")
+    Optional<DossierMedical> findById(Long id);
 }
 
