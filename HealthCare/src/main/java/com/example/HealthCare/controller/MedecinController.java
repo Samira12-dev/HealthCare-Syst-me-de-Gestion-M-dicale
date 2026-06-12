dc83e28c610746a8b818a46899bd517466d196d0
Samira12-dev <samiraelboussidi663@gmail.com>
2026-06-12 13:06:24 +0100

    some modifcations

--- HealthCare/src/main/java/com/example/HealthCare/controller/MedecinController.java
+++ HealthCare/src/main/java/com/example/HealthCare/controller/MedecinController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +21,7 @@ import java.util.List;
 public class MedecinController {
     private  final MedecinService medecinService;
 
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     @Operation(summary = "ajouter medecin")
     public MedecinResponseDTO addMedecin(@Valid  @RequestBody MedecinRequestDTO medecinrequestdto){
